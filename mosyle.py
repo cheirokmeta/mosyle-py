123f14b51950623ad21cedb0f8959195a0a367e1
root <68611844+mdmadmin@users.noreply.github.com>
2021-02-12 08:09:10 +0900

    Update mosyle.py

--- mosyle.py
+++ mosyle.py
@@ -1,82 +1,69 @@
 import requests, json, argparse
-from json2html import *
 
-def mosyle_handler(body_json):
+def mosyleRequest(body_json):
+    # post request settings
     url = 'https://businessapi.mosyle.com/v1/devices'
     headers = { 
         'Content-type': 'application/json', 
         'accesstoken': ''
     }
+    # body_json is json data from argumentDefinitions
     json_in = body_json
+    # send request to api and print results
     response = requests.post(url, json=json_in, headers=headers)
     response_json = response.json()
     dumped_response = json.dumps(response_json, indent=1)
-    return dumped_response
+    print(dumped_response)
 
-def shutdownDevice(device):
-    body = {
-        "operation": 'shutdown_devices',
-        "devices": device
+def argumentDefinitions(command, device):
+    def shutdownDevice(device):
+        body = {
+            "operation": 'shutdown_devices',
+            "devices": device
+            }
+        return body
+    def restartDevice(device):
+        body = {
+            "operation": 'restart_devices',
+            "devices": device
+            }
+        return body
+    def infoDevice(device):
+        body = { 
+            "operation": "list", 
+            "options":
+            {
+                "os": "ios",
+                "deviceudids": device 
+            }
         }
-    return body
+        return body
     
-def restartDevice(device):
-    body = {
-        "operation": 'restart_devices',
-        "devices": device
+    # argument -> function definition map
+    handler = {
+        'shutdown': shutdownDevice,
+        'restart': restartDevice,
+        'info': infoDevice,
         }
-    return body
-
-def infoDevice(device):
-    body = { 
-        "operation": "list", 
-        "options":
-        {
-            "os": "ios",
-            'deviceudids': device 
-        }
-    }
-    return body
+    
+    return handler[command](device)
 
 def main():
-    # Argument definition
-    parser = argparse.ArgumentParser(
-        description='## Mosyle API wrapper',
-        prog='mosyle', 
-        usage='%(prog)s [options] device')
-    # Restart argument
-    parser.add_argument(
-        '--restart', 
-        help='restart device', 
-        action='store_true')
-    # Shutdown argument
-    parser.add_argument(
-        '--shutdown', 
-        help='shutdown device', 
-        action='store_true')
-    # Info argument
-    parser.add_argument(
-        '--info', 
-        help='get device info', 
-        action='store_true')
-    # Device argument
-    parser.add_argument(
-        'device', 
-        metavar='device')
+    # argparse definition
+    parser = argparse.ArgumentParser(description='## Mosyle API wrapper', prog='mosyle', usage='%(prog)s [options] device')
+    # command argument definition
+    parser.add_argument('--command', help='command to send to target device', type=str, required=True)
+    # device argument definition
+    parser.add_argument('--device', help='target device', type=str, required=True)
     
+    # store parser values in a dictionary
     args = vars(parser.parse_args())
     
-    handler = {
-        'shutdown': shutdownDevice, 
-        'restart': restartDevice,
-        'info': infoDevice
-    }
+    # gets json body for api request based on --command arg
+    command_data = argumentDefinitions(args['command'], args['device'])
     
-    for key,value in args.items():
-        if value == True:
-          command = key
-          device = args['device']
-    command_data = handler[command](device)
-    command_results = mosyle_handler(command_data)
-    print(command_results)
+    # send json body to mosyle api request function
+    mosyleRequest(command_data)
+    
 main()
+
